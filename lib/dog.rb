214a7cf14845b02728967b52dff91dbdc7b9d6ef
Nicole R <nicole.m.rogers1@gmail.com>
2019-09-28 00:21:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def initialize(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
+  end
+ 
+  def breed=(dog_breed)
+    @name = dog_name
+    @breed = dog_breed
+  end
+ 
+  def breed
+    @name
+    @breed
+  end
+end
